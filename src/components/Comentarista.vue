69b1754fc4595a1f63f0c336e44720e3edce142d
Antonio V S Pontes <anvictorsantos@gmail.com>
2023-05-15 21:08:28 -0300

    feat: first commit

---
+++ src/components/Comentarista.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="comentar">
+            Comentar
+        </button>
+        <p>
+            {{ comentario }}
+        </p>
+        <p>
+            {{ detalhes }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class Comentarista extends Vue {
+    private comentario = ''
+    @Prop() private nome: string
+
+    private comentar () : void {
+      this.comentario = 'Hello World!'
+    }
+
+    get detalhes () {
+        return `Nome do comentarista: ${this.nome}`
+    }
+}
+
+</script>
